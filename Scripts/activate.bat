c418d416a2c462d32d82560750af0da510d10b9a
JawadArman96 <jawadarman738@gmail.com>
2025-02-06 14:16:04 +0900

    Change in venv

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jawad\pythonWebApp\prayer-time-web\prayer-time-web
+set VIRTUAL_ENV=C:\Users\jawad\pythonWebApp\jawadarman\SalatTimeTable
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(prayer-time-web) %PROMPT%
+set PROMPT=(SalatTimeTable) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(prayer-time-web) 
+set VIRTUAL_ENV_PROMPT=(SalatTimeTable) 
 
 :END
 if defined _OLD_CODEPAGE (
