22259d132ba0d5e21252c2d4e299e85a99e76023
JawadArman96 <jawadarman738@gmail.com>
2025-03-03 21:52:38 +0900

    Add user login, logout feature

--- salat/salattimetable/urls.py
+++ salat/salattimetable/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     # Example route for the home page
     # path('', views.home, name='home'),
     path('', views.dashboard, name='dashboard'),
+    path('login/', views.login_view, name='login_view'),
+    path("logout/", views.logout_view, name="logout"),
     path('prayertime/', views.home, name='home'),
     path('products/', views.product_list, name='product_list'),
 ]
--- salat/salattimetable/views.py
+++ salat/salattimetable/views.py
@@ -1,11 +1,31 @@
 import json
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from .prayer_shedule import PrayerSchedule
 from .models import UserAccounts
 from django.template import loader
 from django.http import HttpResponse
-# from .models import Task
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("dashboard")  # Change "home" to your desired redirect URL
+        else:
+            messages.error(request, "Invalid username or password")
+    return render(request, "login.html")
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, "You have been logged out successfully.")
+    return redirect("login_view")  # Redirect to login page after logout
 
 
 def home(request):
@@ -67,5 +87,8 @@ def dashboard(request):
     return render(request, "dashboard.html", {
         "useracc" : desired_user 
     })
+
+
+    
 
 
